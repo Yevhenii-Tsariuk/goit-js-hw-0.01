d600af315e7195e8dfd704ed7ceafc38daf1413b
Yevhenii-Tsariuk <care64678@gmail.com>
2024-04-28 20:32:46 +0300

    add some changes

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,12 @@
 function getElementWidth(content, padding, border){
-    const Npx = content + padding + padding + border + border;
-    return `${Npx}`;
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+    
+    return totalWidth;
 }
 
-console.log(getElementWidth(50, 8, 4)); // 74
-console.log(getElementWidth(60, 12, 8.5)); // 101
-console.log(getElementWidth(200, 0, 0)); // 200
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
